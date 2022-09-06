88e32acb2cec3974c13cd04c195a70faafc1b6ea
LuisMathias20 <luiscraft15@gmail.com>
2022-09-06 20:42:48 -0300

    Aula 3 - Buscando registro de pedidos do banco

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,24 +3,24 @@ package br.com.alura.mvc.mudi.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	PedidoRepository pedidoRepository;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Notebook Lenovo");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/51GqHrxFv2S._AC_SL1000_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Notebook-ideapad-i5-10300H-256GBSSD-82CGS00100/dp/B0945D7C9T?ref_=Oct_d_obs_d_16364755011&pd_rd_w=7JWO3&content-id=amzn1.sym.574d7dde-c8d1-436d-a7fc-f2654a8c3190&pf_rd_p=574d7dde-c8d1-436d-a7fc-f2654a8c3190&pf_rd_r=YANFEBTP10JJN20PWXSC&pd_rd_wg=JpMC3&pd_rd_r=b30dac0a-3e60-41a5-9647-72cce6b28a49&pd_rd_i=B0945D7C9T");
-		pedido.setDescricao("Silencioso e não esquenta: projetado com um sistema de resfriamento otimizado composto por 2 coolers e 4 saídas de ar para suportar o alto desempenho do notebook");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		List<Pedido> pedidos = pedidoRepository.findAll();
+
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
