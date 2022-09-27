fc8ec707b3b2cd19079b94a9ae60231fc302b806
LuisMathias20 <luiscraft15@gmail.com>
2022-09-27 20:28:59 -0300

    Aula 10 - Criando paginacao

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package br.com.alura.mvc.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,5 +21,5 @@ public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 	@Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username")
 	public List<Pedido> findAllByUsuario(String username);
 
-	public List<Pedido> findByStatus(StatusPedido entregue);
+	public List<Pedido> findByStatus(StatusPedido entregue, Pageable paginacao);
 }
